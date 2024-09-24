e9662a1690a1407ed0fc856b8c759b69f0e48826
Andrea Ross <andreaross.ibt@gmail.com>
2024-09-24 12:55:54 -0700

    Calculo del ΔCt

--- expresion.py
+++ expresion.py
@@ -20,6 +20,30 @@ import seaborn
 from scipy import stats 
 
 # Carga de los datos
-file = '/Users/andreaross/Desktop/bioinfo_andrea/ds.xlsx' # Ruta del archivo con los datos de expresión génica
+file = '/Users/andreaross/Desktop/bioinfo_andrea/ds.csv' # Ruta del archivo con los datos de expresión génica
 df = pd.read_csv(file)
-df.head()
+
+# Definición de los grupos experimentales
+control = 2
+housekeeping = 'mir16'
+target = 'mir141'
+
+# Eliminación de los valores NaN
+df_copy = df[['grupo', 'mir16' , 'mir141']].copy()
+df_copy.dropna(inplace=True)
+df_copy.reset_index(drop=True, inplace=True)
+
+# Cálculo de CT mean para el gen de referencia y el gen de interés
+def ct_mean(df):
+    df_control = df[df['grupo'] == control]
+    
+    ct_mean_housekeeping = round(np.mean(df_control[housekeeping]),3)
+    ct_mean_target = round(np.mean(df_control[target]),3)
+
+    return ct_mean_housekeeping, ct_mean_target
+    
+print(ct_mean(df_copy))
+
+# Cálculo del ΔCt
+
+
